860876815e539d6a19bd4c6e56fdc680d52023ff
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2025-11-26 19:31:35 -0300

    Mod Login

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Prueba de Merge</h1>
+      <h1>Merge Desde RamaTres</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
